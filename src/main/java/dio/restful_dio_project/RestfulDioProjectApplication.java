919bc959de6d945aad627dfd4507cc6909a336f3
Gustavobvs1 <gustavobvs06@gmail.com>
2025-06-18 13:58:27 -0300

    first commit

---
+++ src/main/java/dio/restful_dio_project/RestfulDioProjectApplication.java
@@ -0,0 +1,13 @@
+package dio.restful_dio_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulDioProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulDioProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/restful_dio_project/RestfulDioProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.restful_dio_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulDioProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
